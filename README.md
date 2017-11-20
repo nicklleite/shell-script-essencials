5bfcef62fc20518a610709b7e4e348b86ef3b9ac
Nicholas Leite <nicholas@bptech.com.br>
2017-11-20 18:37:14 -0200

    README e install-composer.sh

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 Repositório criado para reunir scripts que facilitam tarefas do dia a dia.
 
-## Lista de scripts
+### Lista de scripts
 
-- **install-phpci.sh**: Script para instalar o PHPCI
+- **install-phpci.sh**: Script para instalar e configurar o PHPCI.
 - *Link: [PHPCI](https://www.phptesting.org/)* | *[PHPCI - GitHub](https://github.com/block8/phpci)*
+
+---
+
+- **install-composer.sh**: Script para instalar o Composer.
+- *Link: [Composer](https://getcomposer.org/)*
 
 ### Info
 Testado no Fedora 25
---
+++ install-composer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Rotina para download e instalação do Composer
+function installComposer() {
+	echo "Executando: cd ~"
+	cd ~/
+
+	# Executando comandos para instalação do composer
+	echo "Executando: php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\""
+	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
+	echo "Executando: php -r \"if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;\""
+	php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
+	echo "Executando: php composer-setup.php"
+	php composer-setup.php
+
+	echo "Executando: php -r \"unlink('composer-setup.php');\""
+	php -r "unlink('composer-setup.php');"
+
+	# Criando link simbólico
+	ln -s composer.phar /usr/local/bin/composer
+}
+
+# Execução da rotina principal!
+# Here we go...
+installComposer
